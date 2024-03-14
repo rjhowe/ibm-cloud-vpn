396370bbf15445275c50b8ead0ef8942ba18abbb
Ryan Howe <rjhowe@users.noreply.github.com>
2024-03-14 10:44:12 -0400

    Update runit.sh
    Added Prebuilt Image

--- runit.sh
+++ runit.sh
@@ -3,6 +3,6 @@ echo -n Password:
 read -s password
 source <(grep = vpn.ini)
 printf "\n$method $host $user $log\n"
+image=quay.io/rjhowe/ibm-cloud-vpn:9.4.0.42 
 
-sudo podman run --rm -it --name VPNcontainer --privileged --network host ibm-cloud-vpn2.0 --method $method --host $host --user $user --passwd $password --loglevel $log 
-
+sudo podman run --rm -it --name VPNcontainer --privileged --network host $image --method $method --host $host --user $user --passwd $password --loglevel $log 
